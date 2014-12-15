5aa169b047f7f8d8bd17300bfa60aeca3afcfe5f
Rush Tehrani <r@inven.io>
2014-12-15 09:03:32 +0000

    added hooks and Update/Insert methods to db package.

--- db/db.go
+++ db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"reflect"
 	"database/sql"
 
 	"github.com/jmoiron/sqlx"
@@ -17,18 +18,55 @@ func Get(dest interface{}, query string, args ...interface{}) error {
 	return db.Get(dest, query, args...)
 }
 
-func NamedExec(query string, arg interface{}) (sql.Result, error) {
-	return db.NamedExec(query, arg)
+func Select(dest interface{}, query string, args ...interface{}) error {
+	return db.Select(dest, query, args...)
 }
 
 func NamedQuery(query string, arg interface{}) (*sqlx.Rows, error) {
 	return db.NamedQuery(query, arg)
 }
 
-func QueryRowx(query string, args ...interface{}) *sqlx.Row {
-	return db.QueryRowx(query, args)
+func NamedExec(query string, arg interface{}) (sql.Result, error) {
+	return db.NamedExec(query, arg)
 }
 
-func MustBegin() *sqlx.Tx {
-	return db.MustBegin()
+
+func Insert(query string, arg interface{}, primaryKey *int64) error {
+	execHook("PreInsert", arg)
+	
+	rows, err := db.NamedQuery(query, arg) 
+	
+	if err != nil {
+		return err
+	}
+
+	if rows.Next() {
+		rows.Scan(primaryKey)
+	}
+	
+	execHook("PostInsert", arg)
+	
+	return nil
+}
+
+func Update(query string, arg interface{}) error {
+	execHook("PreUpdate", arg)
+	
+	_, err := db.NamedExec(query, arg)
+	
+	if err != nil {
+		return err
+	}
+	
+	execHook("PostUpdate", arg)
+	
+	return nil
+}
+
+func execHook(hookType string, v interface{}) {
+	method := reflect.ValueOf(v).MethodByName(hookType)
+	
+	if method != (reflect.Value{}) {
+		method.Call([]reflect.Value{})
+	}
 }
