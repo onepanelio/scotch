7ba713d6cfb677a109ed4c998099ad18bb31f8c8
rushtehrani <r@inven.io>
2015-03-06 20:36:31 -0800

    introduced squirrel query builder.

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -4,11 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gorilla/context"
+	sq "github.com/lann/squirrel"
 
 	"github.com/rushtehrani/scotch/sql"
 )
 
-var DB *sql.DB
+var (
+	DB *sql.DB
+	Q  sq.StatementBuilderType
+)
 
 func Set(r *http.Request, key, v interface{}) {
 	context.Set(r, key, v)
