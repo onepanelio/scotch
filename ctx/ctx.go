a33a6b9e7a515564de00c57f59bb2fa1c2006e56
rushtehrani <r@inven.io>
2015-03-13 16:24:23 -0700

    removed squirrel sql builder.

--- ctx/ctx.go
+++ ctx/ctx.go
@@ -4,14 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gorilla/context"
-	sq "github.com/lann/squirrel"
 
 	"github.com/rushtehrani/scotch/sql"
 )
 
 var (
 	DB *sql.DB
-	Q  sq.StatementBuilderType
 )
 
 func Set(r *http.Request, key, v interface{}) {
