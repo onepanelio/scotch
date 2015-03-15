aee1c7d23fcb0cd0cd3ee15ec0a41fa5208f3f03
rushtehrani <r@inven.io>
2015-03-15 10:22:59 -0700

    refactored res.Error.

--- res/error.go
+++ res/error.go
@@ -2,6 +2,12 @@ package res
 
 import "net/http"
 
-func Error(w http.ResponseWriter, code int) {
-	http.Error(w, http.StatusText(code), code)
+func Error(w http.ResponseWriter, code int, m ...string) {
+	message := http.StatusText(code)
+
+	if len(m) > 0 {
+		message = m[0]
+	}
+
+	http.Error(w, message, code)
 }
