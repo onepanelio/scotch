e35d020fc8fa9b282452c5d5799fbffc275cc33b
rushtehrani <r@inven.io>
2015-04-08 10:50:17 -0700

    refactored res and added req.

--- res/res.go
+++ res/res.go
@@ -21,3 +21,13 @@ func JSON(w http.ResponseWriter, v interface{}, code ...int) {
 
 	w.Write(js)
 }
+
+func Error(w http.ResponseWriter, code int, m ...string) {
+	message := http.StatusText(code)
+
+	if len(m) > 0 {
+		message = m[0]
+	}
+
+	http.Error(w, message, code)
+}
